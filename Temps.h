e7a542e05ddaa3412a7f9302815789fd42945fe8
Klab93 <37708074+Klab93@users.noreply.github.com>
2020-03-02 19:53:49 +0100

    initialisation des champs et fonctions amies et membres

--- Temps.h
+++ Temps.h
@@ -5,13 +5,61 @@
 #ifndef INF2_LABO1_TEMPS_H
 #define INF2_LABO1_TEMPS_H
 
+#include <ctime>
+#include <iostream>
+
+using uint = unsigned  int;
 
 class Temps {
+    //operateur de comparaison
+    friend bool operator<(const Temps& lhs, const Temps& rhs);
+    friend bool operator>(const Temps& lhs, const Temps& rhs);
+    friend bool operator<=(const Temps& lhs, const Temps& rhs);
+    friend bool operator<=(const Temps& lhs, const Temps& rhs);
+    friend bool operator==(const Temps& lhs, const Temps& rhs);
+    friend bool operator!=(const Temps& lhs, const Temps& rhs);
+
+    //operateur arithmetique
+    friend Temps operator+(Temps lhs, const Temps& rhs);
+    friend Temps operator-(Temps lhs, const Temps& rhs);
+
+    //operateur de flux
+    friend ostream& operator<<(ostream& lhs, const Temps& rhs);
 
 public:
+    //constructor
+    Temps();
+    Temps( time_t temps );
+    Temps( uint heure, uint minute, uint seconde = 0 )
+
+    //getters
+    uint getHeure()   const ;
+    uint getMinute()  const ;
+    uint getSeconde() const ;
+
+    //setters
+    void setHeure( uint heure );
+    void setMinute( uint minute );
+    void setseconde( uint seconde );
+
+    //operateur incrementation et decrementation
+    Temps& operator++();
+    Temps  operator++(int);
+    Temps& operator--();
+    Temps  operator--(int);
+
+    //operateur d'affectation
+    Temps&operator+=(const Temps& rhs);
+    Temps&operator-=(const Temps& rhs);
+
+    //operateur de conversion
+    // a definir
 
 private:
-
+    //champs
+    uint heure;
+    uint minute;
+    uint seconde
 };
 
 
